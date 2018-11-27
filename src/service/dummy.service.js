d84f0e0a161299e2c0c745857e4badcb1e336eba
ilan90 <ilanrot90@gmail.com>
2018-11-27 11:39:56 +0200

    Create dummy.service.js

---
+++ src/service/dummy.service.js
@@ -0,0 +1,10 @@
+function _createDummyText() {
+    return 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto, sed quidem ut dolore iure corrupti expedita. Enim, velit necessitatibus! Mollitia! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto, sed quidem ut dolore iure corrupti expedita. Enim, velit necessitatibus! Mollitia!'                 
+}
+
+export const createDummyData = () => {
+    return {
+
+    }
+}
+
