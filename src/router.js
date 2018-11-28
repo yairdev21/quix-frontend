e79f417583d9a0f238e4c138ba3670e33e2c5415
ilan90 <ilanrot90@gmail.com>
2018-11-28 13:36:25 +0200

    a

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
-import heroA from './components/elements/section-article.cmp.vue'
+import heroA from './components/elements/header-fixed.vue'
 
 
 Vue.use(Router)
