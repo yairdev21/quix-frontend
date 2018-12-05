7612544d03aded9a2b8d486d862527f62c5d49e8
BeBarefoot <35373216+BeBarefoot@users.noreply.github.com>
2018-12-05 17:29:53 +0200

    sidebar fixes

---
+++ src/components/elements/header-fixed.el.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="header">
+    <h1 class="header-title">{{content.title}}</h1>
+    <p class="header-text">{{content.text}}</p>
+  </section>
+</template>
+
+<script>
+import { ID } from "../../services/utils.js";
+
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true
+    },
+    isEditMode: {
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      id: ID(),
+      content: this.data
+    };
+  },
+  created() {
+    document.designMode = this.checkEditMode;
+  },
+  methods: {
+    getContent() {
+      return this.content;
+    },
+    saveText(ev) {
+      this.content = ev.target.innerText;
+      this.data.text = this.content;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  min-height: 55vh;
+  background-size: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &-title {
+    color: #000;
+    margin-bottom: 5.5rem;
+  }
+  &-text {
+    margin: 0 auto;
+    text-align: center;
+    color: #000;
+    font-weight: bold;
+    margin-bottom: 2.5rem;
+  }
+}
+</style>
