611b5236183fc5fbf4dbea7339af62308dea9874
yair Weiss <yair.we21@gmail.com>
2018-12-19 10:23:17 +0200

    id

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,4 +1,7 @@
 export const ID = () => '_' + Math.random().toString(16).substr(2, 24);
-    // Math.random should be unique because of its seeding algorithm.
-    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-    // after the decimal.
+// Math.random should be unique because of its seeding algorithm.
+// Convert it to base 36 (numbers + letters), and grab the first 9 characters
+// after the decimal.
+
+
+// export const ID = () => '_' + Math.random().toString(36).substr(2, 24);
